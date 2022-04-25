faa378a1b4d587ce3192e8ff50e4ef887cc3f4be
Saitarn Dokchan <saitarn14@gmail.com>
2022-04-26 02:11:53 +0700

    day1 completed

---
+++ day1/day1.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+print("What's name of the city you grew up in?")
+country = input()
+print("What's your pet's name?")
+pet_name = input()
+
+print("Your band name could be " + country + " " + pet_name)
